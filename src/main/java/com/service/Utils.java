e39772d685e62ad7baf296babf61931f52398ae9
LRFreelancer <lrfreelancer.mapp@gmail.com>
2017-07-04 08:03:03 +0530

    added simpledate formater to threadlocal

--- src/main/java/com/service/Utils.java
+++ src/main/java/com/service/Utils.java
@@ -6,9 +6,18 @@ import java.util.Date;
 import java.util.TimeZone;
 
 public class Utils {
+	
+	 private static final ThreadLocal<SimpleDateFormat> DATE_FORMATTER = new ThreadLocal<SimpleDateFormat>(){
+	        @Override
+	        protected SimpleDateFormat initialValue()
+	        {
+	            return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+	        }
+	    };
 	public static String getCurrentTime(){
 		TimeZone tz = TimeZone.getTimeZone("UTC");
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'"); 
+		// I am adding formatter into threadlocal to avoid threading issue and memory 
+		DateFormat df = DATE_FORMATTER.get(); 
 		df.setTimeZone(tz);
 		String nowAsISO = df.format(new Date());
 		return nowAsISO;
